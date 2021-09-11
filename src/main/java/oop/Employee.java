d9ecf7b52f0384a993b539dbd1483f8557d1b706
bartnicki <lukasz.bartnicki.external@easy-software.com>
2021-09-11 12:04:25 +0200

    z1 and z2

---
+++ src/main/java/oop/Employee.java
@@ -0,0 +1,67 @@
+package oop;
+
+public class Employee
+{
+	//hermetyzacja
+	private int id;
+	private String name;
+	private String lastname;
+
+	//wartosc ponizszej skladowej bedzie mozna zmieniac
+	private String address;
+
+	//konstruktor definiuje sposob robienia instancji (obiektu) klasy
+
+	public Employee( int id, String name, String lastname )
+	{
+		this.id = id;
+		this.name = name;
+		this.lastname = lastname;
+	}
+
+	//przeciazenie konstruktora (inna wersja konstruktora)
+	public Employee( int id, String name, String lastname, String address )
+	{
+		this.id = id;
+		this.name = name;
+		this.lastname = lastname;
+		this.address = address;
+	}
+
+	//metoda która zwraca true jesli pracownik (obbiekt) na ktorym ta metoda jest wywolywana ma taki sam adres
+	//jak pracownik ktory jest podany metodzie jako argument oraz obaj pracownicy maja ustawione adresy
+	public boolean isTheSameAddress(Employee emp2)
+	{
+		if(this.address == null || emp2.getAddress() == null)
+		{
+			return false;
+		}
+
+		return address.equals( emp2.getAddress() );
+	}
+
+	public String getAddress( )
+	{
+		return address;
+	}
+
+	public void setAddress( String address )
+	{
+		this.address = address;
+	}
+
+	public int getId( )
+	{
+		return id;
+	}
+
+	public String getName( )
+	{
+		return name;
+	}
+
+	public String getLastname( )
+	{
+		return lastname;
+	}
+}
--- src/main/java/oop/Main.java
+++ src/main/java/oop/Main.java
@@ -1,9 +1,74 @@
 package oop;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main
 {
+
+	private static boolean checkPairs(double []values)
+	{
+		int numOfValues = values.length;
+
+		if(numOfValues % 2 != 0) {
+			return false;
+		}
+
+		Set<Double> nums = new HashSet<>(  );
+		for(double x : values) {
+			nums.add( x );
+		}
+
+		System.out.println( nums );
+
+		for(int i =  0; i < numOfValues; i++)
+		{
+			double neg = -1 * values[i];
+			if( !nums.contains( neg ) )  {
+				return false;
+			}
+		}
+		//[-2,1,-1,2] -> true
+		//[1,2,3,4] -> false
+		//[-5,4,5,-4,3] -> false
+
+		//[-4,-2,3,3] -> false
+		//set = (-4, -2, 3 )
+
+		//[-2,1,-1,2] -> true
+		//set = (-2, 1, -1, 2)
+
+		return true;
+
+	}
+
+
+	private static void demoCheckPairs()
+	{
+		double []vals1 = {-4,-2,3,3};
+		System.out.println( checkPairs(vals1) );
+
+		double []vals2 = {-2,1,-1,2};
+		System.out.println( checkPairs(vals2) );
+	}
+
+	private static void demoEmployees()
+	{
+		Employee emp1 = new Employee( 1, "Bartnicki", "Lukasz" );
+		emp1.setAddress( "Krakow ul xyz" );
+		System.out.println(emp1.getAddress()  );
+
+		Employee emp2 = new Employee( 2, "Kasia", "Abcd",  "Lodz www");
+		System.out.println(emp2.getId()  );
+
+		System.out.println( emp1.isTheSameAddress( emp2 ) );
+
+		Employee emp3 = new Employee( 3, "Kasia", "Nowak",  "Lodz www");
+		System.out.println( emp2.isTheSameAddress( emp3 ) );
+	}
+
 	public static void main( String[] args )
 	{
-		System.out.println( "Hello!" );
+		demoEmployees();
 	}
 }
